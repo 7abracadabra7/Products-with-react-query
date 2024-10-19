15b605ef0c7cb3f42224dded4e64afe4ca76c0a1
Nahid Khalili <Nahid_khalili@ymail.com>
2024-10-19 18:02:43 +0330

    registration page design added

--- src/pages/RegistrationForm.jsx
+++ src/pages/RegistrationForm.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../images/Union.png";
+import styles from "./Form.module.css";
 
 const RegistrationForm = () => {
-  return <div>RegistrationForm</div>;
+  return (
+    <div>
+      <div className={styles.container}>
+        <h1 className={styles.header}>بوتکمپ بوتواستارت</h1>
+        <div className={styles.form}>
+          <div className={styles.topSide}>
+            <img src={logo} alt="logo" />
+            <h1>فرم ثبت نام</h1>
+          </div>
+          <div className={styles.inputContainer}>
+            <input type="text" placeholder="نام کاربری" />
+            <input type="password" placeholder="رمز عبور" />
+            <input type="password" placeholder="تکرار رمز عبور" />
+          </div>
+          <button className={styles.formButton}>ثبت نام</button>
+          <div className={styles.rightAlign}>
+            <Link className={styles.link} to="/login">حساب کاربری دارید؟</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RegistrationForm;
