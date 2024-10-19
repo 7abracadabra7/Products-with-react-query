eb57a1c860ce3f177e65f3a622b5f0b2efbddd7f
Nahid Khalili <Nahid_khalili@ymail.com>
2024-10-19 18:09:24 +0330

    loggin page design added

--- src/pages/LoginForm.jsx
+++ src/pages/LoginForm.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import styles from "./Form.module.css";
+import { Link } from "react-router-dom";
+import logo from "../images/Union.png";
 
 const LoginForm = () => {
-  return <div>LoginForm</div>;
+  return (
+    <div>
+      <div className={styles.container}>
+        <h1 className={styles.header}>بوتکمپ بوتواستارت</h1>
+        <div className={styles.form}>
+          <div className={styles.topSide}>
+            <img src={logo} alt="logo" />
+            <h1>فرم ورود</h1>
+          </div>
+          <div className={styles.inputContainer}>
+            <input type="text" placeholder="نام کاربری" />
+            <input type="password" placeholder="رمز عبور" />
+          </div>
+          <button className={styles.formButton}>ورود</button>
+          <div className={styles.rightAlign}>
+            <Link className={styles.link} to="/login">
+              ایجاد حساب کاربری!
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginForm;
